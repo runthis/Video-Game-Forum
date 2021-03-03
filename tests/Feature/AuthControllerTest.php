05d838049f118af08b9aec4fcca4d509378d2ae8
runthis <robertjessemiller@gmail.com>
2021-03-03 11:24:46 -0500

    Updated AuthControllerTest to test logins

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -4,9 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use App\Models\User;
 use Tests\TestCase;
 
@@ -22,11 +20,14 @@ class AuthControllerTest extends TestCase
 		$this->artisan('migrate');
 
 		$this->email = $this->faker->unique()->safeEmail;
+		$this->password = '1234567890';
+		$this->name = 'jsmith_88';
 		$this->model = User::factory()->make([
+			'name' => $this->name,
 			'email' => $this->email,
+			'password' => $this->password,
 		]);
 		$this->user = new UserController($this->model);
-		$this->auth = new AuthController();
 	}
 
 	public function test_can_get_register()
@@ -43,18 +44,13 @@ class AuthControllerTest extends TestCase
 
 	public function test_can_successfully_register()
 	{
-		$username = 'jsmith_88';
-		$email = $this->faker->unique()->safeEmail;
-		$password = $this->faker->password;
-		$confirm_password = $password;
-
 		$response = $this->withoutMiddleware('VerifyCsrfToken')->post(
 			'/registerUser',
 			[
-				'name' => $username,
-				'email' => $email,
-				'password' => $password,
-				'confirm_password' => $confirm_password,
+				'name' => $this->name,
+				'email' => $this->email,
+				'password' => $this->password,
+				'confirm_password' => $this->password,
 			]
 		);
 
@@ -62,23 +58,20 @@ class AuthControllerTest extends TestCase
 
 		$response->assertStatus(302);
 		$response->assertRedirect($expected_redirect);
-		$this->assertDatabaseHas('users', ['name' => $username]);
+		$this->assertDatabaseHas('users', ['name' => $this->name]);
 	}
 
 	public function test_can_fail_register_with_bad_username()
 	{
 		$username = 'jsmith-88';
-		$email = $this->faker->unique()->safeEmail;
-		$password = $this->faker->password;
-		$confirm_password = $password;
 
 		$response = $this->withoutMiddleware('VerifyCsrfToken')->post(
 			'/registerUser',
 			[
 				'name' => $username,
-				'email' => $email,
-				'password' => $password,
-				'confirm_password' => $confirm_password,
+				'email' => $this->email,
+				'password' => $this->password,
+				'confirm_password' => $this->password,
 			]
 		);
 
@@ -87,135 +80,83 @@ class AuthControllerTest extends TestCase
 
 	public function test_can_fail_register_with_bad_email()
 	{
-		$username = 'jsmith_88';
 		$email = 'fake and bad';
-		$password = $this->faker->password;
-		$confirm_password = $password;
 
 		$response = $this->withoutMiddleware('VerifyCsrfToken')->post(
 			'/registerUser',
 			[
-				'name' => $username,
+				'name' => $this->name,
 				'email' => $email,
-				'password' => $password,
-				'confirm_password' => $confirm_password,
+				'password' => $this->password,
+				'confirm_password' => $this->password,
 			]
 		);
 
-		$this->assertDatabaseMissing('users', ['name' => $username]);
+		$this->assertDatabaseMissing('users', ['name' => $this->name]);
 	}
 
 	public function test_can_fail_register_with_non_matching_passwords()
 	{
-		$username = 'jsmith_88';
-		$email = $this->faker->unique()->safeEmail;
-		$password = $this->faker->password;
-		$confirm_password = $this->faker->password;
+		$response = $this->withoutMiddleware('VerifyCsrfToken')->post(
+			'/registerUser',
+			[
+				'name' => $this->name,
+				'email' => $this->email,
+				'password' => $this->password,
+				'confirm_password' => $this->faker->password,
+			]
+		);
+
+		$this->assertDatabaseMissing('users', ['name' => $this->name]);
+	}
+
+	public function test_can_fail_register_with_short_password()
+	{
+		$short_password = 'abc';
 
 		$response = $this->withoutMiddleware('VerifyCsrfToken')->post(
 			'/registerUser',
 			[
-				'name' => $username,
-				'email' => $email,
-				'password' => $password,
-				'confirm_password' => $confirm_password,
+				'name' => $this->name,
+				'email' => $this->email,
+				'password' => $short_password,
+				'confirm_password' => $short_password,
 			]
 		);
 
-		$this->assertDatabaseMissing('users', ['name' => $username]);
+		$this->assertDatabaseMissing('users', ['name' => $this->name]);
 	}
 
-	public function test_can_fail_register_with_bad_everything()
-	{
-		$username = 'jsmith-88';
-		$email = 'fake and bad';
-		$password = $this->faker->password;
-		$confirm_password = $this->faker->password;
-
-		$response = $this->withoutMiddleware('VerifyCsrfToken')->post(
-			'/registerUser',
-			[
-				'name' => $username,
-				'email' => $email,
-				'password' => $password,
-				'confirm_password' => $confirm_password,
-			]
-		);
-
-		$this->assertDatabaseMissing('users', ['name' => $username]);
-	}
-
-	/*
-
-	'name' => 'required|regex:/^[a-z0-9][a-z0-9_]*[a-z0-9]$/i',
-	'email' => 'required|email',
-	'password' => 'required|min:8',
-	'confirm_password' => 'required|same:password'
-
-
-
-	public function test_can_register_user()
-	{
-		$request = new Request;
-
-		$request->merge([
-			'name' => 'longjohnson',
-			'email' => $this->email,
-			'password' => '123456780',
-			'confirm_password' => '123456780'
-		]);
-
-		$this->auth->register_user($request, $this->user);
-
-		//$expected = $this->user->email_exists($this->email);
-
-		//$this->assertTrue($expected);
-	}
-*/
-	/*
-	public function test_cannot_register_user()
+	public function test_can_successfully_login()
 	{
 		$this->model->save();
+		$this->user->register(['name' => $this->name, 'email' => $this->email, 'password' => $this->password]);
 
-		$email = $this->faker->unique()->safeEmail;
-		$expected = $this->user->email_exists($email);
+		$response = $this->withoutMiddleware('VerifyCsrfToken')->post(
+			'/loginUser',
+			[
+				'email' => $this->email,
+				'password' => $this->password
+			]
+		);
 
-		$this->assertFalse($expected);
+		$expected_redirect = url()->getRequest()->getSchemeAndHttpHost();
+
+		$response->assertStatus(302);
+		$response->assertRedirect($expected_redirect);
+		$response->assertSessionHas('user');
 	}
 
-	public function test_can_login()
+	public function test_can_fail_login()
 	{
-		$request = new Request;
+		$response = $this->withoutMiddleware('VerifyCsrfToken')->post(
+			'/loginUser',
+			[
+				'email' => $this->email,
+				'password' => $this->password
+			]
+		);
 
-		$request->merge([
-			'email' => $this->email,
-			'password' => '123456780'
-		]);
-
-		$this->auth->login($request, $this->user);
+		$response->assertSessionMissing('user');
 	}
-
-	public function test_can_get_home()
-	{
-		$this->get_page('/', 'Laravel v8.28.1');
-	}
-
-	public function test_can_get_register()
-	{
-		$this->get_page('/register', 'Forum - Register');
-	}
-
-	public function test_can_get_login()
-	{
-		$this->get_page('/login', 'Forum - Login');
-	}
-
-	private function get_page($page, $see)
-	{
-		$response = $this->get($page);
-
-		$response->assertSuccessful();
-		$response->assertSee($see);
-	}
-	*/
 }
