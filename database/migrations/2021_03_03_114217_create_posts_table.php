0743351670f06b89fcd1e477dc12b686425675bb
runthis <robertjessemiller@gmail.com>
2021-03-03 11:53:17 -0500

    Created schemas for forum posts and replies

---
+++ database/migrations/2021_03_03_114217_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('posts', function (Blueprint $table) {
+			$table->id();
+			$table->integer('owner');
+			$table->string('ip', 45);
+			$table->string('subject', 255);
+			$table->string('body', 2048);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('posts');
+	}
+}
